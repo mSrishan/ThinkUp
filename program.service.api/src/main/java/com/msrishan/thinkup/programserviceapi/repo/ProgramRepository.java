41a775637680f49d70b239346032472df94a1087
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-12-22 19:58:07 +0530

    complete program service

--- program.service.api/src/main/java/com/msrishan/thinkup/programserviceapi/repo/ProgramRepository.java
+++ program.service.api/src/main/java/com/msrishan/thinkup/programserviceapi/repo/ProgramRepository.java
@@ -2,6 +2,8 @@ package com.msrishan.thinkup.programserviceapi.repo;
 
 import com.msrishan.thinkup.programserviceapi.entity.Program;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProgramRepository extends MongoRepository<Program, String> {
 }
--- program.service.api/src/main/java/com/msrishan/thinkup/programserviceapi/service/ProgramService.java
+++ program.service.api/src/main/java/com/msrishan/thinkup/programserviceapi/service/ProgramService.java
@@ -2,6 +2,7 @@ package com.msrishan.thinkup.programserviceapi.service;
 
 import com.msrishan.thinkup.programserviceapi.dto.RequestProgramDto;
 
+
 public interface ProgramService {
     public void createProgram(RequestProgramDto programDto);
 }
