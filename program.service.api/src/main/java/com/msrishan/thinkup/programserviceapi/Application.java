8dae7c9b3517edb6a74ae3a970e8fb497e14073c
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2024-12-26 16:46:30 +0530

    add eureka server

--- program.service.api/src/main/java/com/msrishan/thinkup/programserviceapi/Application.java
+++ program.service.api/src/main/java/com/msrishan/thinkup/programserviceapi/Application.java
@@ -2,12 +2,14 @@ package com.msrishan.thinkup.programserviceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
--- subject.service.api/src/main/java/com/mSrishan/ThinkUp/subject/service/api/Application.java
+++ subject.service.api/src/main/java/com/mSrishan/ThinkUp/subject/service/api/Application.java
@@ -2,12 +2,13 @@ package com.mSrishan.ThinkUp.subject.service.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient; // Instead of EnableEurekaClient
 
 @SpringBootApplication
+@EnableDiscoveryClient  // Use this for Spring Boot 3.x+ (instead of EnableEurekaClient)
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
